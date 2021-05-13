a73e0a14aefb90c3c20e60c68b7889f6d7c7d1fc
KwanL00 <Lihungkwanlhk72>
2021-05-14 02:16:21 +1000

    div slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+let startIndex = 0;
+showSlides(startIndex);
 
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[n].style.display = "block";
+  dots[n].className += " active";
+}
 
 
